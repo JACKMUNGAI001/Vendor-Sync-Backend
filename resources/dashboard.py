3071b20f8c9a0da64176fad81140cd9e6dbbc02a
Jackson <jacksonmungai001@gmail.com>
2025-10-27 21:27:20 +0300

    Complete dashboard endpoint with role-based data

--- resources/dashboard.py
+++ resources/dashboard.py
@@ -1,10 +1,18 @@
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models.user import User
+from models.purchase_order import PurchaseOrder
+from models.quote import Quote
 
 class Dashboard(Resource):
     @jwt_required()
     def get(self):
         user = User.query.get(get_jwt_identity())
         data = []
+        if user.role.name == 'manager':
+            data = [{'id': o.id, 'description': f'Order #{o.id} - {o.status}'} for o in PurchaseOrder.query.filter_by(manager_id=user.id).all()]
+        elif user.role.name == 'staff':
+            data = [{'id': o.order.id, 'description': f'Order #{o.order.id} - {o.order.status}'} for o in user.assignments]
+        elif user.role.name == 'vendor':
+            data = [{'id': o.id, 'description': f'Order #{o.id} - {o.status}'} for o in PurchaseOrder.query.filter_by(vendor_id=user.id).all()]
         return data, 200
