165e42a75c5318703b2caf48b12119c4ee32ce11
Jackson <jacksonmungai001@gmail.com>
2025-10-28 14:52:08 +0300

    update db, User, and Vendor imports to use backend package paths

--- backend/models/purchase_order.py
+++ backend/models/purchase_order.py
@@ -1,6 +1,6 @@
-from app import db
-from models.user import User
-from models.vendor import Vendor
+from backend.app import db
+from backend.models.user import User
+from backend.models.vendor import Vendor
 from datetime import datetime
 
 class PurchaseOrder(db.Model):
